894616c92861f713fadc665b1e0d1b8fdb6ad649
Kirill-68 <kir_54@icloud.com>
2025-04-05 10:59:20 +0700

    Add files via upload

---
+++ Figma/admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Администратор</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="sbmit1">
+    <form action="" method="POST">
+        <label for="administrator" class="admin">Администратор</label>
+        <label for="lastname" class="lastname">Петрова</label>
+        <label for="firstname" class="firstname">Екатерина</label>
+        <label for="username" class="username">Андреевна</label>
+            <label for="lastname2" class="lastname2">Иванова</label>
+            <label for="firstname2" class="firstname2">Анастасия</label>
+            <label for="username2" class="username2">Викторовна</label>
+        <div class="submitt">
+        <input type="submit" value="Удалить" class="delete">
+</div>
+<div class="submit2">
+        <input type="submit" value="Удалить" class="delete2">
+</div>
+<div class="submit3">
+        <input type="submit" value="Изменить" class="redirect">
+        <div>
+            <div class="submit4">
+        <input type="submit" value="Изменить" class="redirect2">
+</div>
+    </form>
+</div>
+</body>
+
+</html>
---
+++ Figma/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="passwor1">
+    <div class="llp">
+    <div class="l">
+        <form action="" method="GET" class="method">
+        <label for="login" class="loginn">Введите логин
+            <span class="error">Необходимо заполнить</span>
+</label>
+</div>
+<div class="ll">
+<label for="password" class="passwordd">Введите пароль
+            <span class="error">Необходимо заполнить</span>
+            </label>
+</div>
+<div class="pd">
+            <label for="password" class="passwordd2">Подтвердите пароль
+            <span class="error">Необходимо заполнить</span>
+            </label>
+</div>
+<div class="p">
+        <input type="text" name="password" class="password">
+</div>
+<div class="passwor2">
+        <input type="text" name="password2" class="password2">
+</div>
+<div class="log">
+        <input type="text" name="login" class="login">
+</div>
+</div>
+<input type="submit" value="Войти" class="submit">
+</form>
+</div>
+</body>
+</html>
